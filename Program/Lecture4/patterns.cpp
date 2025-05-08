4ad0d6955c9dea6c4fff250bea7719d1b77bb0d2
Kratik Paliwal <kratikpaliwal19@gmail.com>
2025-05-09 01:57:50 +0530

    solved some pattern plus cc

---
+++ Program/Lecture4/patterns.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    // 1234
+    // 1234
+    // 1234
+    // 1234
+    // for(int i=1; i<=n; i++){
+    //     for(int j=1; j<=n; j++){      
+    //         cout<<j<<" ";
+
+    //     }
+    //     cout<<endl;
+    // }
+
+
+    // A B C D
+    // A B C D
+    // A B C D
+    // A B C D
+    // for(int i=0; i<n; i++){
+    //     char ch = 'A';
+    //     for(int j=0; j<n; j++){
+    //         cout<<ch<<" ";
+    //         ch = ch+1;
+
+    //     }
+    //     cout<<endl;
+    // }
+
+    // 123
+    // 456
+    // 789
+
+    // for(int i=0; i<n; i++){
+    //     for(int j=(i*n)+1; j<=(n*i)+n; j++){
+    //         cout<<j<<" ";
+
+    //     }
+    //     cout<<endl;
+    // }
+
+
+    // A B C 
+    // D E F 
+    // G H I 
+//     char ch='A';
+//    for(int i=0; i<n; i++){
+//         for(int j=(i*n)+1; j<=(n*i)+n; j++){
+//             cout<<ch<<" ";
+//             ch++;
+
+//         }
+//         cout<<endl;
+//     }
+
+
+
+    // * 
+    // * * 
+    // * * * 
+    // for(int i=0; i<n; i++){
+    //     for(int j=0; j<=i; j++){
+    //         cout<<"*"<<" ";
+    //     }cout<<endl;
+    // }
+
+
+    // 1 
+    // 2 2 
+    // 3 3 3 
+    // for(int i=1; i<=n; i++){
+    //     for(int j=0; j<i; j++){
+    //         cout<<i<<" ";
+    //     }
+    //     cout<<endl;
+
+    // }
+    
+
+    // A 
+    // B B 
+    // C C C 
+    // D D D D 
+    // char ch = 'A';
+    // for(int i=1; i<=n; i++){
+    //     for(int j=0; j<i; j++){
+    //         cout<<ch<<" ";
+    //     }
+    //     cout<<endl;
+    //     ch++;
+    // }
+
+    // 1 
+    // 1 2 
+    // 1 2 3 
+    // 1 2 3 4 
+    for(int i=0; i<n; i++){
+        for(int j=0; j<=i; j++){
+            cout<<j+1<<" ";
+        }
+        cout<<endl;
+    }
+
+
+  
+}
