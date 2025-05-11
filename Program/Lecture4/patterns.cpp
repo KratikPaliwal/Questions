b9ed7d605146f8890416c37d8918dc8b00ac5109
Kratik Paliwal <kratikpaliwal19@gmail.com>
2025-05-11 23:12:31 +0530

    Solvved another question

--- Program/Lecture4/patterns.cpp
+++ Program/Lecture4/patterns.cpp
@@ -97,12 +97,12 @@ int main(){
     // 1 2 
     // 1 2 3 
     // 1 2 3 4 
-    for(int i=0; i<n; i++){
-        for(int j=0; j<=i; j++){
-            cout<<j+1<<" ";
-        }
-        cout<<endl;
-    }
+    // for(int i=0; i<n; i++){
+    //     for(int j=0; j<=i; j++){
+    //         cout<<j+1<<" ";
+    //     }
+    //     cout<<endl;
+    // }
 
 
   
