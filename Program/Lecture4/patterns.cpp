e82b3e99b52579bcc97d3d10d92997952dac8832
Kratik Paliwal <kratikpaliwal19@gmail.com>
2025-05-13 23:59:56 +0530

    Solved

--- Program/Lecture4/patterns.cpp
+++ Program/Lecture4/patterns.cpp
@@ -105,5 +105,17 @@ int main(){
     // }
 
 
+    // 1 
+    // 2 1 
+    // 3 2 1 
+    // 4 3 2 1 
+    for(int i=0; i<n; i++){
+        for(int j=i+1;j>=1; j--){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+
+
   
 }
