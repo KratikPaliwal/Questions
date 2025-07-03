40d1fefd46cf3368ae9cca884e99eeace2794268
Kratik Paliwal <kratikpaliwal19@gmail.com>
2025-07-03 12:20:31 +0530

    Added more problem

---
+++ practice_Problem/prime_in_range.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int count=0;
+    for(int i=2; i<n; i++){
+        for(int j=2; j<i; j++){
+            if(i%j==0){
+                break;
+            }
+        }
+        count++;
+    }
+    cout<<count<<endl;
+}
