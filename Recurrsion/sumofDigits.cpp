bf69bfbf2897310fb9a810e585991225a2ad0e17
Kratik Paliwal <kratikpaliwal19@gmail.com>
2025-09-16 14:15:03 +0530

    Added accepted

---
+++ Recurrsion/sumofDigits.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std; 
+
+int sumOfDigits(int n){
+    if(n==0){
+        return 0;
+    }
+    int rem=n%10;
+    return rem+sumOfDigits(n/10);
+}
+int main(){
+    int n;
+    cin>>n; 
+    int ans=sumOfDigits(n);
+    cout<<ans<<endl;
+    
+
+}
